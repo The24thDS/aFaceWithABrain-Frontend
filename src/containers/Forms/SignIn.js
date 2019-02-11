a8e6abb38193e55483e5cfe9eca51f511ae4c258
David Sima <davidsima@windowslive.com>
2019-02-11 12:59:46 +0200

    Refactoring SignIn validation

--- src/containers/Forms/SignIn.js
+++ src/containers/Forms/SignIn.js
@@ -7,7 +7,7 @@ class SignIn extends React.Component{
         super(props)
         this.state = {
             email: '',
-            password: ''
+            password: '',
         }
     }
 
@@ -17,43 +17,22 @@ class SignIn extends React.Component{
         })
     }
 
-    validEmail = (email) => {
-        const regex = new RegExp(/^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-]+$/)
-        return regex.test(email)
-    }
-    validPassword = (password) => {
-        const regex = new RegExp(/^(?=.*\d)(?=.*[.,<>?'"[\]{}`~!@#$%^&*()\-+_/\\])(?=.*[a-z])(?=.*[A-Z])(?!.*\s).{8,35}$$/) // eg: testPassword.1
-        return regex.test(password)
-    }
-
-    verify = (event) => {
-        if(event.target.name==='email')
+    validate= (event, regex) => {
+		const data = event.target.value
+        if(regex.test(data))
         {
-            if(this.validEmail(event.target.value))
-                {
-                    this.addInfo(event)
-                    event.target.classList.add(styles.valid)
-                }
-            else
-                event.target.classList.add(styles.invalid)
-            return true
+            this.addInfo(event)
+            event.target.classList.add(styles.valid)
         }
-        if(event.target.name==='password')
-        {
-            if(this.validPassword(event.target.value))
-                {
-                    this.addInfo(event)
-                    event.target.classList.add(styles.valid)
-                }
-            else
-                event.target.classList.add(styles.invalid)
-            return true
+        else {
+            event.target.classList.add(styles.invalid)
         }
     }
 
     onSubmit = async(event) => {
         event.preventDefault()
-        if(!this.validEmail(this.state.email) || !this.validPassword(this.state.password))
+        const { email, password } = this.state
+        if(!email.length || !password.length)
             return false;
         const request = {
             method: 'POST',
@@ -76,10 +55,12 @@ class SignIn extends React.Component{
     }
 
     render(){
+        const email = new RegExp(/^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-]+$/)
+        const password = new RegExp(/^(?=.*\d)(?=.*[.,<>?'"[\]{}`~!@#$%^&*()\-+_/\\])(?=.*[a-z])(?=.*[A-Z])(?!.*\s).{8,35}$$/) // eg: testPassword.1
         return(
             <form className={`${styles.form} ${styles.login}`}>
-                <Input onBlur={this.verify} name="email" type="email" text="Email" required="true" />
-                <Input onBlur={this.verify} name="password" type="password" text="Password" required="true" />
+                <Input onBlur={event=>this.validate(event, email)} name="email" type="email" text="Email" required="true" />
+                <Input onBlur={event=>this.validate(event, password)} name="password" type="password" text="Password" required="true" />
                 <Input onClick={this.onSubmit} name="submit" type="submit" text="Log In"/>
             </form>
         )
